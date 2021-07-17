4fbd2620460f827b1b3717bd20bf39ad93ec8ce5
Matthew <67207999+matthewJ1981@users.noreply.github.com>
2021-07-17 13:40:53 -0400

    test std::rotate

--- codeWars/codeWars/bw_transform.cpp
+++ codeWars/codeWars/bw_transform.cpp
@@ -1,9 +1,14 @@
 #include "bw_transform.h"
 
-#include <string>
-#include <utility>
+#include <iostream>
+#include <vector>
 
 std::pair<std::string, int> encode(const std::string& s) {
+
+	std::vector<std::string> matrix;
+	std::string str = s;
+	std::rotate(str.rbegin(), str.rbegin() + 1, str.rend());
+	std::cout << str;
 	return { "", 0 };
 }
 
--- codeWars/codeWars/main.cpp
+++ codeWars/codeWars/main.cpp
@@ -2,25 +2,10 @@
 //
 
 #include <iostream>
-#include "snail_map.h" 
+#include "bw_transform.h" 
 
 int main()
 {
-	std::vector<std::vector<int>> v;
-	v = { {1,2,3,4}, {12,13,14,5}, {11,16,15,6}, {10,9,8,7} };
-	v = { {1,2,3}, {8,9,4}, {7,6,5} };
-	v = { {1,2}, {4,3} };
-	v = { {1} };
-
-	auto result = snail(v);
-
-	for (auto i : result)
-	{
-		std::cout << i;
-		if (i != result.back())
-			std::cout << ", ";
-	}
-
-	std::cout << "\n";
+	encode("Hello");
 		
 }		
